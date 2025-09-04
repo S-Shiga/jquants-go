3d056a7d1df55911b487f9aa34607298032c7d06
Satoshi Shiga <shiga800939@gmail.com>
2025-09-04 23:04:47 +0900

    add codes

---
+++ codes/codes.go
@@ -0,0 +1,102 @@
+package codes
+
+const (
+	SectionTSE1st      = "TSE1st"
+	SectionTSE2nd      = "TSE2nd"
+	SectionMothers     = "TSEMothers"
+	SectionJASDAQ      = "TSEJASDAQ"
+	SectionPrime       = "TSEPrime"
+	SectionStandard    = "TSEStandard"
+	SectionGrowth      = "TSEGrowth"
+	SectionTokyoNagoya = "TokyoNagoya"
+
+	Sector33FisheryAgricultureAndForestry              = "0050"
+	Sector33Mining                                     = "1050"
+	Sector33Construction                               = "2050"
+	Sector33Foods                                      = "3050"
+	Sector33TextilesAndApparels                        = "3100"
+	Sector33PulpAndPaper                               = "3150"
+	Sector33Chemicals                                  = "3200"
+	Sector33Pharmaceutical                             = "3250"
+	Sector33OilAndCoalProducts                         = "3300"
+	Sector33RubberProducts                             = "3350"
+	Sector33GlassAndCeramicsProducts                   = "3400"
+	Sector33IronAndSteel                               = "3450"
+	Sector33NonferrousMetals                           = "3500"
+	Sector33MetalProducts                              = "3550"
+	Sector33Machinery                                  = "3600"
+	Sector33ElectricAppliances                         = "3650"
+	Sector33TransportationEquipment                    = "3700"
+	Sector33PrecisionInstruments                       = "3750"
+	Sector33OtherProducts                              = "3800"
+	Sector33ElectricPowerAndGas                        = "4050"
+	Sector33LandTransportation                         = "5050"
+	Sector33MarineTransportation                       = "5100"
+	Sector33AirTransportation                          = "5150"
+	Sector33WarehousingAndHarborTransportationServices = "5200"
+	Sector33InformationAndCommunication                = "5250"
+	Sector33WholesaleTrade                             = "6050"
+	Sector33RetailTrade                                = "6100"
+	Sector33Banks                                      = "7050"
+	Sector33SecuritiesAndCommodityFutures              = "7100"
+	Sector33Insurance                                  = "7150"
+	Sector33OtherFinancingBusiness                     = "7200"
+	Sector33RealEstate                                 = "8050"
+	Sector33Services                                   = "9050"
+	Sector33Others                                     = "9999"
+)
+
+var Sections = []string{
+	SectionPrime,
+	SectionStandard,
+	SectionGrowth,
+	SectionTokyoNagoya,
+}
+
+var SectionsAll = []string{
+	SectionTSE1st,
+	SectionTSE2nd,
+	SectionMothers,
+	SectionJASDAQ,
+	SectionPrime,
+	SectionStandard,
+	SectionGrowth,
+	SectionTokyoNagoya,
+}
+
+var Sector33Codes = []string{
+	Sector33FisheryAgricultureAndForestry,
+	Sector33Mining,
+	Sector33Construction,
+	Sector33Foods,
+	Sector33TextilesAndApparels,
+	Sector33PulpAndPaper,
+	Sector33Chemicals,
+	Sector33Pharmaceutical,
+	Sector33OilAndCoalProducts,
+	Sector33RubberProducts,
+	Sector33GlassAndCeramicsProducts,
+	Sector33IronAndSteel,
+	Sector33NonferrousMetals,
+	Sector33MetalProducts,
+	Sector33Machinery,
+	Sector33ElectricAppliances,
+	Sector33TransportationEquipment,
+	Sector33PrecisionInstruments,
+	Sector33OtherProducts,
+	Sector33ElectricPowerAndGas,
+	Sector33LandTransportation,
+	Sector33MarineTransportation,
+	Sector33AirTransportation,
+	Sector33WarehousingAndHarborTransportationServices,
+	Sector33InformationAndCommunication,
+	Sector33WholesaleTrade,
+	Sector33RetailTrade,
+	Sector33Banks,
+	Sector33SecuritiesAndCommodityFutures,
+	Sector33Insurance,
+	Sector33OtherFinancingBusiness,
+	Sector33RealEstate,
+	Sector33Services,
+	Sector33Others,
+}
